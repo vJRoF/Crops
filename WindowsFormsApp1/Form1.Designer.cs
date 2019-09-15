d52b41d46daea40fb9f725d9233f27d2ebc04552
Utenkov Vladimir <vladimir.utenkov@gmail.com>
2019-09-15 15:42:13 +0300

    Работает с разными формами

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panAndZoomPictureBox1 = new Emgu.CV.UI.PanAndZoomPictureBox();
+            this.tbX0 = new System.Windows.Forms.TextBox();
+            this.tbY0 = new System.Windows.Forms.TextBox();
+            this.tbY1 = new System.Windows.Forms.TextBox();
+            this.tbX1 = new System.Windows.Forms.TextBox();
+            this.tbY2 = new System.Windows.Forms.TextBox();
+            this.tbX2 = new System.Windows.Forms.TextBox();
+            this.tbY3 = new System.Windows.Forms.TextBox();
+            this.tbX3 = new System.Windows.Forms.TextBox();
+            this.ibDst = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.panAndZoomPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibDst)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panAndZoomPictureBox1
+            // 
+            this.panAndZoomPictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panAndZoomPictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.panAndZoomPictureBox1.Name = "panAndZoomPictureBox1";
+            this.panAndZoomPictureBox1.Size = new System.Drawing.Size(475, 450);
+            this.panAndZoomPictureBox1.TabIndex = 0;
+            this.panAndZoomPictureBox1.TabStop = false;
+            this.panAndZoomPictureBox1.Click += new System.EventHandler(this.PanAndZoomPictureBox1_Click);
+            this.panAndZoomPictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PanAndZoomPictureBox1_Paint);
+            // 
+            // tbX0
+            // 
+            this.tbX0.Location = new System.Drawing.Point(481, 12);
+            this.tbX0.Name = "tbX0";
+            this.tbX0.Size = new System.Drawing.Size(34, 22);
+            this.tbX0.TabIndex = 1;
+            this.tbX0.TextChanged += new System.EventHandler(this.TbX_TextChanged);
+            // 
+            // tbY0
+            // 
+            this.tbY0.Location = new System.Drawing.Point(521, 12);
+            this.tbY0.Name = "tbY0";
+            this.tbY0.Size = new System.Drawing.Size(34, 22);
+            this.tbY0.TabIndex = 2;
+            this.tbY0.TextChanged += new System.EventHandler(this.TbY_TextChanged);
+            // 
+            // tbY1
+            // 
+            this.tbY1.Location = new System.Drawing.Point(521, 40);
+            this.tbY1.Name = "tbY1";
+            this.tbY1.Size = new System.Drawing.Size(34, 22);
+            this.tbY1.TabIndex = 4;
+            this.tbY1.TextChanged += new System.EventHandler(this.TbY_TextChanged);
+            // 
+            // tbX1
+            // 
+            this.tbX1.Location = new System.Drawing.Point(481, 40);
+            this.tbX1.Name = "tbX1";
+            this.tbX1.Size = new System.Drawing.Size(34, 22);
+            this.tbX1.TabIndex = 3;
+            this.tbX1.TextChanged += new System.EventHandler(this.TbX_TextChanged);
+            // 
+            // tbY2
+            // 
+            this.tbY2.Location = new System.Drawing.Point(521, 68);
+            this.tbY2.Name = "tbY2";
+            this.tbY2.Size = new System.Drawing.Size(34, 22);
+            this.tbY2.TabIndex = 6;
+            this.tbY2.TextChanged += new System.EventHandler(this.TbY_TextChanged);
+            // 
+            // tbX2
+            // 
+            this.tbX2.Location = new System.Drawing.Point(481, 68);
+            this.tbX2.Name = "tbX2";
+            this.tbX2.Size = new System.Drawing.Size(34, 22);
+            this.tbX2.TabIndex = 5;
+            this.tbX2.TextChanged += new System.EventHandler(this.TbX_TextChanged);
+            // 
+            // tbY3
+            // 
+            this.tbY3.Location = new System.Drawing.Point(521, 96);
+            this.tbY3.Name = "tbY3";
+            this.tbY3.Size = new System.Drawing.Size(34, 22);
+            this.tbY3.TabIndex = 8;
+            this.tbY3.TextChanged += new System.EventHandler(this.TbY_TextChanged);
+            // 
+            // tbX3
+            // 
+            this.tbX3.Location = new System.Drawing.Point(481, 96);
+            this.tbX3.Name = "tbX3";
+            this.tbX3.Size = new System.Drawing.Size(34, 22);
+            this.tbX3.TabIndex = 7;
+            this.tbX3.TextChanged += new System.EventHandler(this.TbX_TextChanged);
+            // 
+            // ibDst
+            // 
+            this.ibDst.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ibDst.Location = new System.Drawing.Point(561, 0);
+            this.ibDst.Name = "ibDst";
+            this.ibDst.Size = new System.Drawing.Size(239, 450);
+            this.ibDst.TabIndex = 2;
+            this.ibDst.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ibDst);
+            this.Controls.Add(this.tbY3);
+            this.Controls.Add(this.tbX3);
+            this.Controls.Add(this.tbY2);
+            this.Controls.Add(this.tbX2);
+            this.Controls.Add(this.tbY1);
+            this.Controls.Add(this.tbX1);
+            this.Controls.Add(this.tbY0);
+            this.Controls.Add(this.tbX0);
+            this.Controls.Add(this.panAndZoomPictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.panAndZoomPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibDst)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.PanAndZoomPictureBox panAndZoomPictureBox1;
+        private System.Windows.Forms.TextBox tbX0;
+        private System.Windows.Forms.TextBox tbY0;
+        private System.Windows.Forms.TextBox tbY1;
+        private System.Windows.Forms.TextBox tbX1;
+        private System.Windows.Forms.TextBox tbY2;
+        private System.Windows.Forms.TextBox tbX2;
+        private System.Windows.Forms.TextBox tbY3;
+        private System.Windows.Forms.TextBox tbX3;
+        private Emgu.CV.UI.ImageBox ibDst;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panAndZoomPictureBox1.Image = Image.FromFile("hqdefault.jpg");
+        }
+
+        private void Transform()
+        {
+            if (_corners.Count != 4)
+                return;
+
+            using (Image<Bgr, Byte> src = new Image<Bgr, byte>(new Bitmap(panAndZoomPictureBox1.Image)))
+            using (Image<Bgr, Byte> dst = new Image<Bgr, byte>(GetSizeFromCorners()))
+            {
+                var transformationMatrix = CvInvoke.GetPerspectiveTransform(
+                    new PointF[] { _corners[0], _corners[1], _corners[2], _corners[3] },
+                    new PointF[] { new PointF(0, 0), new PointF(dst.Width, 0), new PointF(dst.Width, dst.Height), new PointF(0, dst.Height) });
+                CvInvoke.WarpPerspective(src, dst, transformationMatrix, dst.Size);
+
+                ibDst.Image = dst;
+            }
+        }
+
+        private List<PointF> _corners = new List<PointF>();
+
+        private Size GetSizeFromCorners()
+        {
+            var topLength = GetLength(_corners[1], _corners[0]);
+            var rightLengt = GetLength(_corners[2], _corners[1]);
+            var bottomLengt = GetLength(_corners[3], _corners[2]);
+            var leftLengt = GetLength(_corners[0], _corners[3]);
+            return new Size(Convert.ToInt32((topLength + bottomLengt) / 2), Convert.ToInt32((rightLengt + leftLengt) / 2));
+        }
+
+        private float GetLength(PointF point1, PointF point2)
+        {
+            return Convert.ToSingle(
+                Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2)));
+        }
+
+        private static float _radius = 5.0f;
+        private static Pen _scaffoldsPen = new Pen(Color.FromArgb(128, Color.Red), 1);
+        private void DrawScaffolding(System.Drawing.Graphics g)
+        {
+            foreach (var corner in _corners)
+            {
+                g.DrawEllipse(_scaffoldsPen, corner.X - _radius / 2, corner.Y - _radius / 2, _radius, _radius);
+            }
+
+            for (int i = 1; i < _corners.Count + 1; i++)
+                g.DrawLine(_scaffoldsPen, _corners[(i - 1) % _corners.Count], _corners[i % _corners.Count]);
+        }
+
+        private void PanAndZoomPictureBox1_Click(object sender, EventArgs e)
+        {
+            var mouseEventArgs = (MouseEventArgs)e;
+            //_corners.Add(new PointF(mouseEventArgs.X, mouseEventArgs.Y));
+            _corners.Add(GetRealPoint(mouseEventArgs.X, mouseEventArgs.Y));
+            panAndZoomPictureBox1.Invalidate();
+        }
+
+        private RectangleF _clipBounds;
+
+        private void PanAndZoomPictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            _clipBounds = e.Graphics.ClipBounds;
+            DrawScaffolding(e.Graphics);
+            PrintCoordinates();
+            Transform();
+        }
+
+        private void PrintCoordinates()
+        {
+            for (int i = 0; i < 4; i++)
+                if(_corners.Count > i )
+                {
+                    this.Controls[$"tbX{i}"].Text = Convert.ToString(_corners[i].X);
+                    this.Controls[$"tbY{i}"].Text = Convert.ToString(_corners[i].Y);
+                }
+        }
+
+        private PointF GetRealPoint(int x, int y)
+        {
+            var realX = x / panAndZoomPictureBox1.ZoomScale + _clipBounds.X;
+            var realY = y / panAndZoomPictureBox1.ZoomScale + _clipBounds.Y;
+
+            return new PointF((float)realX, (float)realY);
+        }
+
+        private void TbX_TextChanged(object sender, EventArgs e)
+        {
+            var control = sender as Control;
+            if (string.IsNullOrWhiteSpace(control?.Text))
+                return;
+
+            var index = int.Parse(Regex.Match(control?.Name, @"\d+").Value);
+            var value = float.Parse(Regex.Match(control?.Text, @"\d+").Value);
+            _corners[index] = new PointF(value, _corners[index].Y);
+            panAndZoomPictureBox1.Invalidate();
+        }
+
+        private void TbY_TextChanged(object sender, EventArgs e)
+        {
+            var control = sender as Control;
+            if (string.IsNullOrWhiteSpace(control?.Text))
+                return;
+
+            var index = int.Parse(Regex.Match(control?.Name, @"\d+").Value);
+            var value = float.Parse(Regex.Match(control?.Text, @"\d+").Value);
+            _corners[index] = new PointF(_corners[index].X, value);
+            panAndZoomPictureBox1.Invalidate();
+        }
+    }
+}
