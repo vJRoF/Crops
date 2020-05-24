eb3ca9f6a82d58d08b705de5d30e9e3e87819c4b
Utenkov Vladimir <vladimir.utenkov@gmail.com>
2020-05-24 15:29:30 +0300

    something to commit

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+            // something to commit
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
