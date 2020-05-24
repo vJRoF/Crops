81d9e528dc1e9dcc371f9201a517c076fcea94e5
Utenkov Vladimir <vladimir.utenkov@gmail.com>
2020-05-24 15:34:40 +0300

    remove something to commit

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -14,7 +14,6 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
-            // something to commit
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
